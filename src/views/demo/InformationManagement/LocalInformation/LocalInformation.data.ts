6368ee59f536069a043bac1cba57bce3ae48c98c
zhuoml <zhuoml@qq.com>
2022-04-23 16:16:47 +0800

    feat: 本地信息

---
+++ src/views/demo/InformationManagement/LocalInformation/LocalInformation.data.ts
@@ -0,0 +1,108 @@
+import { BasicColumn } from '/@/components/Table';
+import { FormSchema } from '/@/components/Table';
+import { h } from 'vue';
+import { Switch } from 'ant-design-vue';
+import { setRoleStatus } from '/@/api/demo/system';
+import { useMessage } from '/@/hooks/web/useMessage';
+
+export const columns: BasicColumn[] = [
+  {
+    title: '标题',
+    dataIndex: 'title',
+    width: 120,
+  },
+  {
+    title: '描述',
+    dataIndex: 'description',
+    width: 120,
+  },
+  {
+    title: '图片',
+    dataIndex: 'pictures',
+    width: 120,
+  },
+  {
+    title: '状态',
+    dataIndex: 'status',
+    width: 80,
+    customRender: ({ record }) => {
+      if (!Reflect.has(record, 'pendingStatus')) {
+        record.pendingStatus = false;
+      }
+      return h(Switch, {
+        checked: record.status === true,
+        checkedChildren: '已启用',
+        unCheckedChildren: '已禁用',
+        loading: record.pendingStatus,
+        onChange(checked: boolean) {
+          record.pendingStatus = true;
+          const newStatus = checked ? true : false;
+          const { createMessage } = useMessage();
+          setRoleStatus(record.id, newStatus)
+            .then(() => {
+              record.status = newStatus;
+              createMessage.success(`已成功修改状态`);
+            })
+            .catch(() => {
+              createMessage.error('修改状态失败');
+            })
+            .finally(() => {
+              record.pendingStatus = false;
+            });
+        },
+      });
+    },
+  },
+  {
+    title: '新增时间',
+    dataIndex: 'created_at',
+    width: 120,
+  },
+  {
+    title: '更新时间',
+    dataIndex: 'updated_at',
+    width: 120,
+  },
+];
+
+export const searchFormSchema: FormSchema[] = [
+  {
+    field: 'title',
+    label: '标题 ',
+    component: 'Input',
+    colProps: { span: 6 },
+  },
+  {
+    field: 'status',
+    label: '状态',
+    component: 'Select',
+    componentProps: {
+      options: [
+        { label: '启用', value: true },
+        { label: '停用', value: false },
+      ],
+    },
+    colProps: { span: 4 },
+  },
+];
+
+export const formSchema: FormSchema[] = [
+  {
+    field: 'id',
+    label: 'ID',
+    component: 'Input',
+    show: false,
+  },
+  {
+    field: 'full_name',
+    label: '姓名',
+    required: true,
+    component: 'Input',
+  },
+  {
+    field: 'phone',
+    label: '手机号',
+    required: true,
+    component: 'Input',
+  },
+];
