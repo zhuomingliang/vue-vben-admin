2fde9128839a7aed6b41e2a8a0accef9e5e7be43
zhuoml <zhuoml@qq.com>
2022-04-28 11:44:39 +0800

    feat: 服务指南

--- src/views/demo/InformationManagement/ServiceInformation/TrafficInformation.data.ts
+++ src/views/demo/InformationManagement/ServiceInformation/TrafficInformation.data.ts
@@ -1,21 +1,57 @@
 import { BasicColumn } from '/@/components/Table';
 import { FormSchema } from '/@/components/Table';
+import { h } from 'vue';
+import { Switch } from 'ant-design-vue';
+import { putStatus } from '/@/api/demo/RideArrangements';
+import { useMessage } from '/@/hooks/web/useMessage';
 
 export const columns: BasicColumn[] = [
   {
-    title: '姓名',
-    dataIndex: 'full_name',
+    title: '类型',
+    dataIndex: 'type',
     width: 120,
   },
   {
-    title: '手机号',
-    dataIndex: 'phone',
+    title: '标题',
+    dataIndex: 'title',
     width: 120,
   },
   {
-    title: '添加渠道',
-    dataIndex: 'from',
+    title: '图片',
+    dataIndex: 'pictures',
     width: 120,
+  },
+  {
+    title: '状态',
+    dataIndex: 'status',
+    width: 80,
+    customRender: ({ record }) => {
+      if (!Reflect.has(record, 'pendingStatus')) {
+        record.pendingStatus = false;
+      }
+      return h(Switch, {
+        checked: record.status === true,
+        checkedChildren: '已启用',
+        unCheckedChildren: '已禁用',
+        loading: record.pendingStatus,
+        onChange(checked: boolean) {
+          record.pendingStatus = true;
+          const newStatus = checked ? true : false;
+          const { createMessage } = useMessage();
+          putStatus(record.id, newStatus)
+            .then(() => {
+              record.status = newStatus;
+              createMessage.success(`已成功修改状态`);
+            })
+            .catch(() => {
+              createMessage.error('修改状态失败');
+            })
+            .finally(() => {
+              record.pendingStatus = false;
+            });
+        },
+      });
+    },
   },
   {
     title: '新增时间',
@@ -37,10 +73,16 @@ export const searchFormSchema: FormSchema[] = [
     colProps: { span: 4 },
   },
   {
-    field: 'phone',
-    label: '手机号',
-    component: 'Input',
-    colProps: { span: 6 },
+    field: 'status',
+    label: '状态',
+    component: 'Select',
+    componentProps: {
+      options: [
+        { label: '启用', value: true },
+        { label: '停用', value: false },
+      ],
+    },
+    colProps: { span: 4 },
   },
 ];
 
@@ -62,5 +104,17 @@ export const formSchema: FormSchema[] = [
     label: '手机号',
     required: true,
     component: 'Input',
+  },
+  {
+    field: 'status',
+    label: '状态',
+    component: 'RadioButtonGroup',
+    defaultValue: false,
+    componentProps: {
+      options: [
+        { label: '启用', value: true },
+        { label: '停用', value: false },
+      ],
+    },
   },
 ];
