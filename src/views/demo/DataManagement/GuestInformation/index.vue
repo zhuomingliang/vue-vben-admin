5f8b499413b7b0feb8879dd4fa28425bc4ca960b
zhuoml <zhuoml@qq.com>
2022-06-10 10:06:13 +0800

    feat: 首页信息

--- src/views/demo/DataManagement/GuestInformation/index.vue
+++ src/views/demo/DataManagement/GuestInformation/index.vue
@@ -12,14 +12,6 @@
               icon: 'clarity:note-edit-line',
               onClick: handleEdit.bind(null, record),
             },
-            {
-              icon: 'ant-design:delete-outlined',
-              color: 'error',
-              popConfirm: {
-                title: '是否确认删除',
-                confirm: handleDelete.bind(null, record),
-              },
-            },
           ]"
         />
       </template>
