39ca1f5acf77e48b3947ec859c958728b988f094
zhuoml <zhuoml@qq.com>
2022-09-20 15:45:09 +0800

    fix: 完善代码

---
+++ src/views/demo/NavigationManagement/Navigation/NavigationModal3.vue
@@ -0,0 +1,71 @@
+<template>
+  <BasicModal
+    v-bind="$attrs"
+    @register="registerModal"
+    :title="getTitle"
+    @ok="handleSubmit"
+    width="500px"
+  >
+    <BasicForm @register="registerForm" />
+  </BasicModal>
+</template>
+<script lang="ts">
+  import { defineComponent, ref, computed, unref } from 'vue';
+  import { BasicModal, useModalInner } from '/@/components/Modal';
+  import { BasicForm, useForm } from '/@/components/Form/index';
+  import { formSchema3 } from './Navigation.data';
+  import { postThirdMenu, putThirdMenu } from '/@/api/demo/Navigation';
+
+  export default defineComponent({
+    name: 'NavigationModal',
+    components: { BasicModal, BasicForm },
+    emits: ['success', 'register'],
+    setup(_, { emit }) {
+      const isUpdate = ref(true);
+      const rowId = ref('');
+
+      const [registerForm, { setFieldsValue, resetFields, validate }] = useForm({
+        labelWidth: 100,
+        schemas: formSchema3,
+        showActionButtonGroup: false,
+        actionColOptions: {
+          span: 23,
+        },
+      });
+
+      const [registerModal, { setModalProps, closeModal }] = useModalInner(async (data) => {
+        resetFields();
+        setModalProps({ confirmLoading: false });
+        isUpdate.value = !!data?.isUpdate;
+
+        const record = data.record;
+        //if (unref(isUpdate)) {
+        setFieldsValue({
+          ...record,
+        });
+        //}
+      });
+
+      const getTitle = computed(() => (!unref(isUpdate) ? '新增' : '编辑'));
+
+      async function handleSubmit() {
+        try {
+          const values = await validate();
+          setModalProps({ confirmLoading: true });
+          if (unref(isUpdate)) {
+            await putThirdMenu(values);
+          } else {
+            delete values.id;
+            await postThirdMenu(values);
+          }
+          closeModal();
+          emit('success', { isUpdate: unref(isUpdate), values: { ...values, id: rowId.value } });
+        } finally {
+          setModalProps({ confirmLoading: false });
+        }
+      }
+
+      return { registerModal, registerForm, getTitle, handleSubmit };
+    },
+  });
+</script>
